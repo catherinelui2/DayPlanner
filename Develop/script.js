819e096a57f74732f47d41bfb712fc296bc04a68
CatL <catherinelui.2@gmail.com>
2020-07-25 22:08:33 -0600

    Finish product

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,82 @@
 // allows users to save events for each hour
-let text = JSON.parse(localStorage.getItem("#text")) || [];
-let lastID = localStorage.getItem("lastID") || 0;
-
-$('#button-addon2').click(function(event) {
-    event.preventDefault();
-    const newItem = {
-        id: ++lastID,
-        event: $('#text').val().trim()
+const savedToDoStorageKey = "savedToDos";
+let savedToDos = JSON.parse(localStorage.getItem(savedToDoStorageKey)) || {};
+const timeSlotsContainer = $('#timeSlotsContainer');
+const beginningHour24 = 8;
+const endingHour24 = 5 + 12;
+const currentDateTime = moment().subtract(8, 'hour'); 
+for (let slotHour24 = beginningHour24; slotHour24 <= endingHour24; slotHour24++) {
+    let slotHour12;
+    let slotMeridian; 
+    if (slotHour24 > 12) {
+        slotHour12 = slotHour24 - 12;
+        slotMeridian = "pm";
     }
-    if (!newItem.event) {
-        return alert("Must enter a task/event in order to save!")
+    else {
+        slotHour12 = slotHour24;
+        slotMeridian = "am";
     }
 
-    text.push(newItem);
-    updateStorage();
-    text.text(newItem.event);
+    let currentSlotClassName;
+    let currentHr24 = currentDateTime.hour();
+    if (currentHr24 === slotHour24) {
+        currentSlotClassName = "currentHrTimeSlot";
+    } else if (currentHr24 < slotHour24 ) {
+        currentSlotClassName = "futureHrTimeSlot";
+    } else {
+        currentSlotClassName = "pastHrTimeSlot";
+    }
 
-});
+
+    const timeSlotHr = slotHour12 + slotMeridian;
+    const timeSlotHTML = createTimeSlotHtml(timeSlotHr);
+    let timeSlotEl = $($.parseHTML(timeSlotHTML));
+    timeSlotEl.addClass(currentSlotClassName);
+    let textAreaEl = timeSlotEl.find("textarea");
+    let savedToDo = savedToDos[timeSlotHr];
+    if (savedToDo) {
+        textAreaEl.val(savedToDo);
+    }
+    let buttonEl = timeSlotEl.find("button");
+    timeSlotsContainer.append(timeSlotEl);
 
 
+
+    buttonEl.click(function (event) {
+        event.preventDefault();
+        const newItem = textAreaEl.val().trim();
+
+        if (!newItem) {
+            return alert("Must enter a task/event in order to save!");
+        }
+
+        savedToDos[timeSlotHr] = newItem;
+        updateStorage();
+    });
+
+    
+}
+
+
+function createTimeSlotHtml(timeSlotHr) {
+    const timeSlotHTML = `
+    <div id="input-${timeSlotHr}" class="input-group">
+        <div class="input-group-prepend">
+            <span class="input-group-text">${timeSlotHr}</span>
+        </div>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+        <button class="btn btn-outline-secondary" type="button" class="button-addon2">Save</button>
+    </div>`;
+    return timeSlotHTML;
+};
 
 const updateStorage = () => {
-    localStorage.setItem("text", JSON.stringify(text));
-    localStorage.setItem("lastID", JSON.stringify(lastID));
-}
+    localStorage.setItem(savedToDoStorageKey, JSON.stringify(savedToDos));
+};
 // Add important events to a daily planner
-// current day is displayed at the top 
-var currentDate = moment().format('LLLL');
-$('#currentDay').append(currentDate);
+// current day is displayed at the top
+
+$("#currentDay").append(currentDateTime.format("LLLL"));
 // scroll to present timeblocks for standard business hours
 // timeblocks color coded to indicate whether it is the past, present or future
 // click timeblock to edit or enter event
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ scroll to present timeblocks for standard business hours
 timeblocks color coded to indicate whether it is the past, present or future
 click timeblock to edit or enter event
 click save button on timeblock to local storage and keep content on timeblock
+
+
